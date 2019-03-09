9c92905ee9bb8f4e72412ee98c2ed149a80f1d87
Aulia Ahmad Sormin <auliaahmad165@gmail.com>
2019-03-09 13:24:00 +0700

    Update unfollnotfollback.js

--- tools/unfollnotfollback.js
+++ tools/unfollnotfollback.js
@@ -147,7 +147,7 @@ const Excute = async function(User,sleep,mysyntx){
 console.log(chalk`{bold.green
   Ξ TITLE  : UNFB *UNFOLLOW NOT FOLLOWBACK INSTAGRAM!
   Ξ CODE   : CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  Ξ UPDATE : ADD ITTYW by @officialputuid
+  Ξ UPDATE : ADD ITTYW by @auliaahmad16
   Ξ STATUS : ITTYW : {bold.green Supported!}}
       `);
 inquirer.prompt(User)
