b6012cb8634cc1ad087e5a44f456c95a99155b86
Aulia Ahmad Sormin <auliaahmad165@gmail.com>
2019-03-09 13:21:00 +0700

    Update botlike.js

--- tools/botlike.js
+++ tools/botlike.js
@@ -97,7 +97,7 @@ const Excute = async function(User, sleep){
 console.log(chalk`{bold.green
   Ξ TITLE  : BOTLIKE TL v1 *Set Sleep.
   Ξ CODE   : CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  Ξ UPDATE : ADD ITTYW by @officialputuid
+  Ξ UPDATE : ADD ITTYW by @auliaahmad16
   Ξ STATUS : ITTYW : {bold.red Not Supported!}}
   `);
 
