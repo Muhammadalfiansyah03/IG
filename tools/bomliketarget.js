ea47c934f692d87b359c22e45d4f95d10510bce3
Aulia Ahmad Sormin <auliaahmad165@gmail.com>
2019-03-09 13:20:24 +0700

    Update bomliketarget.js

--- tools/bomliketarget.js
+++ tools/bomliketarget.js
@@ -154,7 +154,7 @@ const Excute = async function(User, TargetUsername, mysyntx, sleep){
 console.log(chalk`{bold.green
   Ξ TITLE  : BOM LIKE TARGET *Set Sleep.
   Ξ CODE   : CYBER SCREAMER CCOCOT (ccocot@bc0de.net)
-  Ξ UPDATE : ADD ITTYW by @officialputuid
+  Ξ UPDATE : ADD ITTYW by @auliaahmad16
   Ξ STATUS : ITTYW : {bold.green Supported!}}
       `);
 inquirer.prompt(User)
